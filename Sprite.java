93c97da895af3b89c0479aae2abacd751df8cf14
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-04-27 01:20:57 -0400

    Update javadoc Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -1,4 +1,7 @@
-public class Sprite extends Bebida {
+/*Clase Sprite
+ * @author Gustavo Benítez
+ * @version versión 1, 25 de abril 2024*/
+class Sprite extends Bebida {
     public Sprite() {
         super("Sprite", 120);
     }
